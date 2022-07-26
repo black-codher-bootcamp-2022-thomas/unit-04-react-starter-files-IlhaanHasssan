49b19b8c2301480a2cabe06e11483bd13e13f75a
Ilhaan <IlhaanHassanA@outlook.com>
2022-07-26 20:06:31 +0100

    button element created

--- session01/index.js
+++ session01/index.js
@@ -1,1 +1,28 @@
 // Add the code here
+function ClickButton() {
+
+    const [clicked, setClick] = React.useState(false);
+  
+    if (clicked) {
+      return 'Nothing here yet! Change this text to add more info.';
+    }
+  
+    return React.createElement(
+      'button', {
+      style: {
+        backgroundColor: "#3392e4",
+        color: "#fff",
+        border: "0",
+        padding: "10px 20px",
+        fontWeight: "bold",
+        borderRadius: "5px",
+        fontSize: "1.1em"
+      },
+      onClick: () => setClick(true)
+    },
+      'Click To Reveal'
+    );
+  }
+  
+  const domContainer = document.querySelector('#root');
+  ReactDOM.render(React.createElement(ClickButton), domContainer);
